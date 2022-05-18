297cf2d569a6fc3f22b0d778680e82c1983f757f
StephonT <Stephonfresh@aol.com>
2022-05-18 16:52:10 -0400

    added conditional functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,51 @@
 var playerName = window.prompt("What is your robot's name?");
-// Note the lack of quotation marks around playerName
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+console.log(playerName, playerHealth, playerAttack); //You can also log multiple values at once like this
 
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+//Enemy Data
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 //declared fight function
-function fight() {
+var fight = function () {
+  //Alert players that a round has started
   window.alert("The fight has begun!");
-}
 
-//fight();
+  // Subtract the value of playerAttack from enemyHealth then update the value in enemyHealth variable
+  enemyHealth = enemyHealth - playerAttack;
+  //playerName attacked enemyName. enemyName now has enemyHealth left
+  console.log(
+    playerName +
+      " attacked " +
+      enemyName +
+      " . " +
+      enemyName +
+      " now has " +
+      enemyHealth +
+      " health remaining."
+  );
+  // Subtract the value of enemyAttack from playerHealth then update the value in playerHealth variable
+  playerHealth = playerHealth - enemyAttack;
+  //enemyName attacked playerName. playerName now has playerHealth left
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      " . " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
+
+  //check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+};
+
+fight();
